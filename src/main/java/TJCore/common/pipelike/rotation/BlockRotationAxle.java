cb379a04b500e1623ba65dbe9f3bf58c953a444e
iAmUnableToSpell <98621396+iAmUnableToSpell@users.noreply.github.com>
2023-01-22 23:35:44 -0500

    Someone other than me make the fucking correct model used. I dont know why mine only uses normal variant. Compare to BlockEndRod, observer, or any other BlockDirectional and youll find that everything is the same. BlockRotationAxle just defaults to normal variant and I can't figure out where that is set. please help me.

--- src/main/java/TJCore/common/pipelike/rotation/BlockRotationAxle.java
+++ src/main/java/TJCore/common/pipelike/rotation/BlockRotationAxle.java
@@ -7,28 +7,54 @@ import net.minecraft.block.material.Material;
 import net.minecraft.block.properties.IProperty;
 import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.init.Blocks;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.BlockRenderLayer;
 import net.minecraft.util.EnumFacing;
+import net.minecraft.util.Mirror;
+import net.minecraft.util.Rotation;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 public class BlockRotationAxle extends BlockDirectional implements ITileEntityProvider {
 
     public BlockRotationAxle() {
         super(Material.IRON);
-        setTranslationKey("rotation_axle");
         this.setDefaultState(this.blockState.getBaseState().withProperty(FACING, EnumFacing.NORTH));
     }
 
     @Override
-    protected BlockStateContainer createBlockState() {
-        return new BlockStateContainer(this, new IProperty[]{FACING});
+    protected @NotNull BlockStateContainer createBlockState() {
+        return new BlockStateContainer(this, FACING);
     }
 
     @Override
     public boolean hasTileEntity(IBlockState state) {
+        state.withProperty(FACING, EnumFacing.NORTH);
         return true;
     }
+
+    public IBlockState withRotation(IBlockState state, Rotation rot) {
+        return state.withProperty(FACING, rot.rotate((EnumFacing)state.getValue(FACING)));
+    }
+
+    public IBlockState withMirror(IBlockState state, Mirror mirrorIn) {
+        return state.withProperty(FACING, mirrorIn.mirror((EnumFacing)state.getValue(FACING)));
+    }
+
+    public boolean isOpaqueCube(IBlockState state) {
+        return false;
+    }
+
+    public boolean isFullCube(IBlockState state) {
+        return false;
+    }
+
 
     @Nullable
     @Override
@@ -36,8 +62,32 @@ public class BlockRotationAxle extends BlockDirectional implements ITileEntityPr
         return new TileEntityRotationAxle();
     }
 
-    @Override
-    public int getMetaFromState(IBlockState state) {
-        return ((EnumFacing) state.getValue(FACING)).getIndex();
+    public int getMetaFromState(IBlockState state)
+    {
+        return ((EnumFacing)state.getValue(FACING)).getIndex();
+    }
+
+    public IBlockState getStateFromMeta(int meta)
+    {
+        IBlockState iblockstate = this.getDefaultState();
+        iblockstate = iblockstate.withProperty(FACING, EnumFacing.byIndex(meta));
+        return iblockstate;
+    }
+
+    @SideOnly(Side.CLIENT)
+    public BlockRenderLayer getRenderLayer() {
+        return BlockRenderLayer.CUTOUT;
+    }
+
+    public @NotNull IBlockState getStateForPlacement(World worldIn, BlockPos pos, EnumFacing facing, float hitX, float hitY, float hitZ, int meta, EntityLivingBase placer) {
+        IBlockState iblockstate = worldIn.getBlockState(pos.offset(facing.getOpposite()));
+        if (iblockstate.getBlock() == Blocks.END_ROD) {
+            EnumFacing enumfacing = (EnumFacing)iblockstate.getValue(FACING);
+            if (enumfacing == facing) {
+                return this.getDefaultState().withProperty(FACING, facing.getOpposite());
+            }
+        }
+
+        return this.getDefaultState().withProperty(FACING, facing);
     }
 }
